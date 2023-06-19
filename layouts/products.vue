8a4412aaab844addab8d8ceefe2656c473227a72
Quynh Ngo <ngophuongquynh25@gmail.com>
2023-06-19 13:22:10 +0200

    custom layout for products

---
+++ layouts/products.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <nuxt-link to="/products">Nuxt Products</nuxt-link>
+        <!-- internal links within Nuxt.js apps: avoiding full page reloads,  automatic active class assignment-->
+
+        <!--<a href="/about">normal link to about page</a>
+            internal and external links:  full page reloads -->
+      </nav>
+    </header>
+    <!-- output content -->
+    <div><slot /></div>
+
+    <footer>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+        <li><nuxt-link to="/products">Products</nuxt-link></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
